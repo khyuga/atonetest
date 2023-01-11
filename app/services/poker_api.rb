9dfbd49644b689832f851c8045fb0978a907c3a1
khyuga <khyuga@netprotections.co.jp>
2023-01-11 17:39:19 +0900

    APIのエラーメッセージ表示形式修正

--- app/services/poker_api.rb
+++ app/services/poker_api.rb
@@ -7,24 +7,27 @@ module PokerAPI
   def api_output(cards_array)
     irregular_cards = cards_array.select{ |cards| PokerError.hand_validation(cards)}
     regular_cards = cards_array - irregular_cards
-
+    
     error = irregular_cards.map do |cards|
-      if PokerError.hand_validation(cards)
+      if PokerError.hand_validation(cards)[1]
         {
           cards: cards,
-          msg: PokerError.hand_validation(cards).join("\n")
+          msg: PokerError.hand_validation(cards)
+        }
+      else
+        {
+          cards: cards,
+          msg: PokerError.hand_validation(cards)[0]
         }
       end
     end
 
     result = regular_cards.map do |cards|
-      if PokerHand.judgement_result(cards)
         {
           cards: cards,
           hand: PokerHand.judgement_result(cards)[0][:name],
           best: PokerHand.judgement_result(cards)[0][:score]
         }
-      end
     end
     result = PokerBest.best_hand(result)
 

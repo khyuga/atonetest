c49fead5bb1b73da1a08f566e7ed7779e2ea4c4a
khyuga <khyuga@netprotections.co.jp>
2023-01-11 12:52:27 +0900

    JSON以外の入力に対応するエラーメッセージの作成

--- app/api/ver1/root.rb
+++ app/api/ver1/root.rb
@@ -3,6 +3,18 @@ module Ver1
     # これでdomain/api/v1でアクセス出来るようになる。(versioning)
     version 'v1', using: :path
     format :json
+
+    default_format :json
+    content_type :json, "application/json"
+    content_type :xml, 'application/xml'
+    content_type :javascript, 'application/javascript'
+    content_type :txt, 'text/plain'
+    content_type :html, 'text/html'
+
+    rescue_from Grape::Exceptions::Base do |e|
+      error!({ errors: [{ msg: "入力形式をJSONに設定してください。" }] }, 400)
+    end
+
     mount Ver1::Cards
   end
 end

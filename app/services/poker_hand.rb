9654c7591ea4c51382f3cff6044c98dd415b1159
khyuga <khyuga@netprotections.co.jp>
2023-01-17 15:50:29 +0900

    各役の判定メソッドを真偽値を返す形に変更、合わせてHAND_LISTも修正

--- app/services/poker_hand.rb
+++ app/services/poker_hand.rb
@@ -1,21 +1,25 @@
 # 指定通りに入力されたカードがどの役に該当するかを判定
 module PokerHand
   HAND_LIST = {
-    straight_flush: { name: 'ストレートフラッシュ', score: 8 },
-    four_card: { name: 'フォーカード', score: 7 },
-    full_house: { name: 'フルハウス', score: 6 },
-    flush: { name: 'フラッシュ', score: 5 },
-    straight: { name: 'ストレート', score: 4 },
-    three_card: { name: 'スリーカード', score: 3 },
-    two_pair: { name: 'ツーペア', score: 2 },
-    one_pair: { name: 'ワンペア', score: 1 },
-    high_card: { name: 'ハイカード', score: 0 }
+    straight_flush?: { name: 'ストレートフラッシュ', score: 8 },
+    four_card?: { name: 'フォーカード', score: 7 },
+    full_house?: { name: 'フルハウス', score: 6 },
+    flush?: { name: 'フラッシュ', score: 5 },
+    straight?: { name: 'ストレート', score: 4 },
+    three_card?: { name: 'スリーカード', score: 3 },
+    two_pair?: { name: 'ツーペア', score: 2 },
+    one_pair?: { name: 'ワンペア', score: 1 },
+    high_card?: { name: 'ハイカード', score: 0 }
   }.freeze
 
   # 役判定メソッド
   def judgement_result(cards)
+    #HAND_LIST.keys.detect do |hand|
+    # HAND_LIST[hand] if send(hand  '?', cards)
+    #end
     hand_list_keys = HAND_LIST.keys.detect{ |x| send(x, cards) }
     HAND_LIST[hand_list_keys]
+    #binding.pry
   end
 
   def suit_array(cards)
@@ -31,56 +35,54 @@ module PokerHand
     num_array.uniq.map { |e| num_array.count(e) }.sort
   end
 
-  def straight_flush(cards)
-    HAND_LIST[:straight_flush] if straight(cards) && flush(cards)
+  def straight_flush?(cards)
+    straight?(cards) && flush?(cards)
   end
 
-  def straight(cards)
+  def straight?(cards)
     num_array = number_array(cards)
-    if (num_array.max - num_array.min == 4 && num_array.uniq.size == 5) || num_array.sort == [1, 10, 11, 12, 13]
-      HAND_LIST[:straight]
-    end
+    (num_array.max - num_array.min == 4 && num_array.uniq.size == 5) || num_array.sort == [1, 10, 11, 12, 13]
   end
 
-  def flush(cards)
-    HAND_LIST[:flush] if suit_array(cards).uniq.size == 1
+  def flush?(cards)
+    suit_array(cards).uniq.size == 1
   end
 
-  def four_card(cards)
-    HAND_LIST[:four_card] if number_duplicate_counts(cards) == [1, 4]
+  def four_card?(cards)
+    number_duplicate_counts(cards) == [1, 4]
   end
 
-  def full_house(cards)
-    HAND_LIST[:full_house] if number_duplicate_counts(cards) == [2, 3]
+  def full_house?(cards)
+    number_duplicate_counts(cards) == [2, 3]
   end
 
-  def three_card(cards)
-    HAND_LIST[:three_card] if number_duplicate_counts(cards) == [1, 1, 3]
+  def three_card?(cards)
+    number_duplicate_counts(cards) == [1, 1, 3]
   end
 
-  def two_pair(cards)
-    HAND_LIST[:two_pair] if number_duplicate_counts(cards) == [1, 2, 2]
+  def two_pair?(cards)
+    number_duplicate_counts(cards) == [1, 2, 2]
   end
 
-  def one_pair(cards)
-    HAND_LIST[:one_pair] if number_duplicate_counts(cards) == [1, 1, 1, 2]
+  def one_pair?(cards)
+    number_duplicate_counts(cards) == [1, 1, 1, 2]
   end
 
-  def high_card(_cards)
-    HAND_LIST[:high_card]
+  def high_card?(cards)
+    number_duplicate_counts(cards) == [1, 1, 1, 1, 1] && ( [straight?(cards)] || [flush?(cards)] == false )
   end
 
   module_function :judgement_result,
                   :suit_array,
                   :number_array,
                   :number_duplicate_counts,
-                  :straight_flush,
-                  :flush,
-                  :straight,
-                  :four_card,
-                  :full_house,
-                  :three_card,
-                  :two_pair,
-                  :one_pair,
-                  :high_card
+                  :straight_flush?,
+                  :flush?,
+                  :straight?,
+                  :four_card?,
+                  :full_house?,
+                  :three_card?,
+                  :two_pair?,
+                  :one_pair?,
+                  :high_card?
 end

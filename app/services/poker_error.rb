351baeee2fbe276cee2d0739855ed69336bfd314
khyuga <khyuga@netprotections.co.jp>
2023-01-18 18:57:42 +0900

    cardsをhandに、handをcardsに変更

--- app/services/poker_error.rb
+++ app/services/poker_error.rb
@@ -1,38 +1,38 @@
 # 指定通りに入力されなかったカードがどのエラーに該当するかを判定
 module PokerError
   # エラー内容を判定する
-  def hand_validation(cards)
-    if cards.blank?
+  def hand_validation(hand)
+    if hand.blank?
       error_message = ['カードを入力してください。']
     else
-      hands = cards.split
-      if hands.size != 5
+      cards = hand.split
+      if cards.size != 5
         error_message = ['5つのカード指定文字を半角スペース区切りで入力してください。（例："S1 H3 D9 C13 S11"）']
-      elsif incorrect_words(hands).present? && duplicate(hands).present?
+      elsif incorrect_words(cards).present? && duplicate(cards).present?
         error_message = []
-        error_message << incorrect_words(hands)
-        error_message << duplicate(hands)
+        error_message << incorrect_words(cards)
+        error_message << duplicate(cards)
         error_message.flatten
       else
-        incorrect_words(hands) || duplicate(hands)
+        incorrect_words(cards) || duplicate(cards)
       end
     end
   end
 
   # 不正文字を含むケース
-  def incorrect_words(hands)
+  def incorrect_words(cards)
     error_message = []
     correct_pairs = /^([CDHS])(1[0-3]|[1-9])$/
-    hands.map.with_index do |card, i|
+    cards.map.with_index do |card, i|
       error_message << "#{i + 1}番目のカード指定文字が不正です。（#{card}）" unless card.match(correct_pairs)
     end
     error_message if error_message != []
   end
 
   # 重複を含むケース
-  def duplicate(hands)
-    if hands.uniq.size != 5
-      duplicate_cards = hands.select { |v| hands.count(v) > 1 }.uniq
+  def duplicate(cards)
+    if cards.uniq.size != 5
+      duplicate_cards = cards.select { |v| cards.count(v) > 1 }.uniq
       error_message = ["カードが重複しています。（#{duplicate_cards.join(', ')}）"]
     end
   end

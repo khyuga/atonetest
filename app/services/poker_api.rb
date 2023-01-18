e4de01d21a12943085f74ec30bd8ec98957bd192
khyuga <khyuga@netprotections.co.jp>
2023-01-18 19:00:39 +0900

    cardsをhandに変更

--- app/services/poker_api.rb
+++ app/services/poker_api.rb
@@ -2,28 +2,28 @@
 module PokerAPI
 
   def api_output(hands)
-    irregular_cards = hands.select { |cards| PokerError.hand_validation(cards)&.any? }
+    irregular_cards = hands.select { |hand| PokerError.hand_validation(hand)&.any? }
     regular_cards = hands - irregular_cards
 
-    error = irregular_cards.map do |cards|
-      if PokerError.hand_validation(cards)[1]
+    error = irregular_cards.map do |hand|
+      if PokerError.hand_validation(hand)[1]
         {
-          cards: cards,
-          msg: PokerError.hand_validation(cards)
+          cards: hand,
+          msg: PokerError.hand_validation(hand)
         }
       else
         {
-          cards: cards,
-          msg: PokerError.hand_validation(cards)[0]
+          cards: hand,
+          msg: PokerError.hand_validation(hand)[0]
         }
       end
     end
 
-    result = regular_cards.map do |cards|
+    result = regular_cards.map do |hand|
       {
-        cards: cards,
-        hand: PokerHand.judgement_result(cards)[:name],
-        best: PokerHand.judgement_result(cards)[:score]
+        cards: hand,
+        hand: PokerHand.judgement_result(hand)[:name],
+        best: PokerHand.judgement_result(hand)[:score]
       }
     end
     result = PokerBest.best_hand(result)

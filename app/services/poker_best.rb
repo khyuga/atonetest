65b8e8b3cbbeb5b98f9ac4e1238a1e33f12f8705
khyuga <khyuga@netprotections.co.jp>
2023-01-20 13:51:29 +0900

    {:cards, :hand, :score}を{:cards, :hand, :best}に書き換える形式に修正

--- app/services/poker_best.rb
+++ app/services/poker_best.rb
@@ -1,9 +1,10 @@
 # 最も強い役に該当するカード組を判定
 module PokerBest
   def best_hand(result)
-    score_array = result.map { |score| score[:best] }
-    score_array.map.with_index do |_score, i|
-      result[i][:best] = (result[i][:best] == score_array.max)
+    scores = result.map { |res| res[:score] }
+    result.map.with_index do |res, i|
+     res[:best] = (result[i][:score] == scores.max)
+     res.delete(:score)
     end
     result
   end

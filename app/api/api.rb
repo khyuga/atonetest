c7e16d51d85adef94afe10c6e8b2f8f5aa257921
khyuga <khyuga@netprotections.co.jp>
2023-01-24 12:08:25 +0900

    [refactor]不要なコメントアウトの削除

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,9 +1,7 @@
 module API
   class Root < Grape::API
-    # http://localhost:3000/api/
     prefix 'api'
     mount API::Ver1::Root
     mount Services::PokerAPI
-    # api/ver1/rootを認識させて使えるようにする
   end
 end

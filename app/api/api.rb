d972164afcd0c2736231439be30067a1316f73f1
khyuga <khyuga@netprotections.co.jp>
2023-01-25 19:27:03 +0900

    feat:APIの仕様をhttp://localhost:3000/api/swagger_docにて公開

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,7 +3,8 @@ module API
     # http://localhost:3000/api/
     prefix 'api'
     mount API::Ver1::Root
-    mount Services::PokerAPI
+    #mount Services::PokerAPI
+    add_swagger_documentation
     # api/ver1/rootを認識させて使えるようにする
   end
 end
--- app/api/ver1/cards.rb
+++ app/api/ver1/cards.rb
@@ -3,8 +3,9 @@ module Ver1
     content_type :json, 'application/json'
     format :json
 
+    desc "複数のカード組を入力し、ポーカーの役と勝敗を判定して返します。"
     params do
-      requires :cards, type: Array
+      requires :cards, type: Array, desc: '入力例：{"cards": ["D1 D13 D12 D11 D10","H9 C9 S9 H2 C2"]}'
     end
 
     resource :poker do
